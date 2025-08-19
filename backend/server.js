30f8dc5ce7f90f4d3a2591baae8feafd7f8d35a1
MyDuon <100708123+MyDuon@users.noreply.github.com>
2025-08-19 12:43:24 +0200

    made a src folder to make structure

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,23 @@
 import express from "express"
+import notesRoutes from "./src/routes/notesRoutes.js"
 
 const app = express()
 
-app.get("/api/notes", (req, res) => {
-    // send the notes
-    res.status(200).send("you got 10 notes")
-})
-app.post("/api/notes", (req, res) => {
-    res.status(201).json({message: "note created successfully"})
-})
-app.put("/api/notes/:id", (req, res) => {
-    res.status(200).json({message: "note updated successfully"})
-})
-app.delete("/api/notes/:id", (req, res) => {
-    res.status(200).json({message: "note deleted successfully"})
-})
+app.use("/api/notes", notesRoutes)
+
+// app.get("/api/notes", (req, res) => {
+//     // send the notes
+//     res.status(200).send("you got 10 notes")
+// })
+// app.post("/api/notes", (req, res) => {
+//     res.status(201).json({message: "note created successfully"})
+// })
+// app.put("/api/notes/:id", (req, res) => {
+//     res.status(200).json({message: "note updated successfully"})
+// })
+// app.delete("/api/notes/:id", (req, res) => {
+//     res.status(200).json({message: "note deleted successfully"})
+// })
 
 app.listen(5001, () => {
     console.log("server started on PORT: 5001")
