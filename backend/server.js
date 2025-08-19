59e52a275a389533e374d0aa6e4f73ec63715dfc
MyDuon <100708123+MyDuon@users.noreply.github.com>
2025-08-19 12:16:44 +0200

    installed nodemon

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,17 @@ import express from "express"
 const app = express()
 
 app.get("/api/notes", (req, res) => {
-    res.send("you got 5 notes")
+    // send the notes
+    res.status(200).send("you got 10 notes")
+})
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({message: "note created successfully"})
+})
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({message: "note updated successfully"})
+})
+app.delete("/api/notes/:id", (req, res) => {
+    res.status(200).json({message: "note deleted successfully"})
 })
 
 app.listen(5001, () => {
