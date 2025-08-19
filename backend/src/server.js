2be947242065ca89ca565e8a3ebb1dbd2af62e09
MyDuon <100708123+MyDuon@users.noreply.github.com>
2025-08-19 21:30:25 +0200

    made env file and started on note

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,21 @@
 import express from "express"
 import notesRoutes from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+console.log(process.env.MONGO_URI)
 
 const app = express()
+const PORT = process.env.PORT || 5001
+
+connectDB();
 
 app.use("/api/notes", notesRoutes)
 
-app.listen(5001, () => {
-    console.log("server started on PORT: 5001")
+app.listen(PORT, () => {
+    console.log("server started on PORT:", PORT)
 })
 
 //mongodb+srv://mydh1502:EFI9VgwiG3gWJ5yA@cluster0.u2bkwmp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
