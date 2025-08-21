7731f42bc96ca8af2290897bf7c2801eb22f4e56
MyDuon <100708123+MyDuon@users.noreply.github.com>
2025-08-21 14:22:13 +0200

    done with backend

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,7 +1,7 @@
 import Note from "../models/Note.js";
-export async function getAllNotes(req, res) {
+export async function getAllNotes(_, res) {
   try {
-    const notes = await Note.find();
+    const notes = await Note.find().sort({ createdAt: -1 }); // show the newest first
     res.status(200).json(notes);
   } catch (error) {
     console.error("Error in getAllNotes controller", error);
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
@@ -10,15 +11,23 @@ console.log(process.env.MONGO_URI);
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-connectDB();
-
 // middelware
-app.use(express.json());
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
+
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// });
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log("server started on PORT:", PORT);
+connectDB.then(() => {
+  // only start server after db is connected
+  app.listen(PORT, () => {
+    console.log("server started on PORT:", PORT);
+  });
 });
 
 //mongodb+srv://mydh1502:EFI9VgwiG3gWJ5yA@cluster0.u2bkwmp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
