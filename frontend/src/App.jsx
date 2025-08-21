e4965a0f90b6ab4ea8d47d6cbdf28a77423d0bf2
MyDuon <100708123+MyDuon@users.noreply.github.com>
2025-08-21 15:23:50 +0200

    initialized the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import NoteDetailPage from "./pages/NoteDetailPage";
+import toast from "react-hot-toast";
+
+const App = () => {
+  return (
+    <div>
+      <button className="btn">Button</button>
+      <button className="btn btn-neutral">Neutral</button>
+      <button className="btn btn-primary">Primary</button>
+      <button className="btn btn-secondary">Secondary</button>
+      <button className="btn btn-accent">Accent</button>
+      <button className="btn btn-ghost">Ghost</button>
+      <button className="btn btn-link">Link</button>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/create" element={<CreatePage />}></Route>
+        <Route path="/note/:id" element={<NoteDetailPage />}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
